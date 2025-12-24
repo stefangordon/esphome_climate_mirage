8fee900af8af20ba1e993ecb33478f1a0b6d9aa6
Stefan Gordon <stefan@stefangordon.com>
2025-12-24 16:42:47 -0500

    Refactor climate.py to include remote components

--- components/mirage/climate.py
+++ components/mirage/climate.py
@@ -1,20 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate_ir
-from esphome.const import CONF_ID
+from esphome.components import climate, climate_ir, remote_transmitter, remote_receiver
+from esphome.const import CONF_ID, CONF_TRANSMITTER_ID, CONF_RECEIVER_ID
 
 AUTO_LOAD = ["climate_ir"]
-CODEOWNERS = ["@glmnet"]
 
 mirage_ns = cg.esphome_ns.namespace("mirage")
 MirageClimate = mirage_ns.class_("MirageClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend(
+# --- MANUAL SCHEMA DEFINITION (Fixes the crash) ---
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(MirageClimate),
+        cv.Optional(CONF_TRANSMITTER_ID): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
+        cv.Optional(CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
     }
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
