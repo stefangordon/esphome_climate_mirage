aea42291154dcdfe8cee684e62b123eab84a5a58
Stefan Gordon <stefan@stefangordon.com>
2025-12-24 16:39:28 -0500

    Update CONFIG_SCHEMA to use CLIMATE_IR_SCHEMA

--- components/mirage/climate.py
+++ components/mirage/climate.py
@@ -9,7 +9,7 @@ CODEOWNERS = ["@glmnet"]
 mirage_ns = cg.esphome_ns.namespace("mirage")
 MirageClimate = mirage_ns.class_("MirageClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
+CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(MirageClimate),
     }
