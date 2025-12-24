3b4e7fa84398a8f887ac477578f9d586ae180a47
Stefan Gordon <stefan@stefangordon.com>
2025-12-24 16:44:36 -0500

    Update climate.py to use string literals for IDs

--- components/mirage/climate.py
+++ components/mirage/climate.py
@@ -1,19 +1,19 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, climate_ir, remote_transmitter, remote_receiver
-from esphome.const import CONF_ID, CONF_TRANSMITTER_ID, CONF_RECEIVER_ID
+from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
 
 mirage_ns = cg.esphome_ns.namespace("mirage")
 MirageClimate = mirage_ns.class_("MirageClimate", climate_ir.ClimateIR)
 
-# --- MANUAL SCHEMA DEFINITION (Fixes the crash) ---
+# We use string literals "transmitter_id" to avoid import errors
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(MirageClimate),
-        cv.Optional(CONF_TRANSMITTER_ID): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
-        cv.Optional(CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
+        cv.Optional("transmitter_id"): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
+        cv.Optional("receiver_id"): cv.use_id(remote_receiver.RemoteReceiverComponent),
     }
 )
 
