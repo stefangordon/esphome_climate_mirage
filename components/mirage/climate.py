716e09eaf69b8596953e4064fc9a28b6975ca860
Stefan Gordon <stefan@stefangordon.com>
2025-12-24 16:47:22 -0500

    Add sensor support to MirageClimate configuration

--- components/mirage/climate.py
+++ components/mirage/climate.py
@@ -1,20 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate, climate_ir, remote_transmitter, remote_receiver
-from esphome.const import CONF_ID
+from esphome.components import climate, climate_ir, remote_transmitter, remote_receiver, sensor
+from esphome.const import CONF_ID, CONF_SENSOR
 
 AUTO_LOAD = ["climate_ir"]
 
 mirage_ns = cg.esphome_ns.namespace("mirage")
 MirageClimate = mirage_ns.class_("MirageClimate", climate_ir.ClimateIR)
 
-# --- THE 2025 COMPATIBLE FIX ---
-# We use the new 'climate_schema' function instead of the old constants
+# --- 2025 COMPATIBLE SCHEMA WITH SENSOR SUPPORT ---
 CONFIG_SCHEMA = climate.climate_schema(MirageClimate).extend(
     {
         cv.GenerateID(): cv.declare_id(MirageClimate),
         cv.Optional("transmitter_id"): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
         cv.Optional("receiver_id"): cv.use_id(remote_receiver.RemoteReceiverComponent),
+        cv.Optional(CONF_SENSOR): cv.use_id(sensor.Sensor),
     }
 )
 
