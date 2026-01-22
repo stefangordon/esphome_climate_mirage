38211a892ec650f6f5c9402135fc1a61a25b91de
Stefan Gordon <stefan@stefangordon.com>
2026-01-22 11:35:53 -0500

    Refactor climate component to use climate_ir

--- components/mirage/climate.py
+++ components/mirage/climate.py
@@ -1,64 +1,14 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import climate, remote_transmitter, remote_receiver, sensor
-from esphome.const import CONF_ID, CONF_SENSOR
+from esphome.components import climate_ir
 
-# We need to load climate_ir at runtime
 AUTO_LOAD = ["climate_ir"]
 CODEOWNERS = ["@stefangordon"]
 
-# Config keys
-CONF_TRANSMITTER_ID = "transmitter_id"
-CONF_RECEIVER_ID = "receiver_id"
-CONF_SUPPORTS_COOL = "supports_cool"
-CONF_SUPPORTS_HEAT = "supports_heat"
-
-# Get the climate_ir namespace and class
-climate_ir_ns = cg.esphome_ns.namespace("climate_ir")
-ClimateIR = climate_ir_ns.class_("ClimateIR", climate.Climate, cg.Component)
-
-# Our component
 mirage_ns = cg.esphome_ns.namespace("mirage")
-MirageClimate = mirage_ns.class_("MirageClimate", ClimateIR)
+MirageClimate = mirage_ns.class_("MirageClimate", climate_ir.ClimateIR)
 
-# Build the schema manually - this is the pattern that works
-CONFIG_SCHEMA = cv.All(
-    climate.CLIMATE_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(MirageClimate),
-            cv.GenerateID(CONF_TRANSMITTER_ID): cv.use_id(
-                remote_transmitter.RemoteTransmitterComponent
-            ),
-            cv.Optional(CONF_RECEIVER_ID): cv.use_id(
-                remote_receiver.RemoteReceiverComponent
-            ),
-            cv.Optional(CONF_SUPPORTS_COOL, default=True): cv.boolean,
-            cv.Optional(CONF_SUPPORTS_HEAT, default=True): cv.boolean,
-            cv.Optional(CONF_SENSOR): cv.use_id(sensor.Sensor),
-        }
-    ).extend(cv.COMPONENT_SCHEMA),
-)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(MirageClimate)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await climate.register_climate(var, config)
-
-    # Connect the transmitter
-    transmitter = await cg.get_variable(config[CONF_TRANSMITTER_ID])
-    cg.add(var.set_transmitter(transmitter))
-
-    # Set supports flags
-    cg.add(var.set_supports_cool(config[CONF_SUPPORTS_COOL]))
-    cg.add(var.set_supports_heat(config[CONF_SUPPORTS_HEAT]))
-
-    # Connect temperature sensor if provided
-    if CONF_SENSOR in config:
-        sens = await cg.get_variable(config[CONF_SENSOR])
-        cg.add(var.set_sensor(sens))
-
-    # Connect receiver if provided
-    if CONF_RECEIVER_ID in config:
-        receiver = await cg.get_variable(config[CONF_RECEIVER_ID])
-        cg.add(receiver.register_listener(var))
+    await climate_ir.new_climate_ir(config)
