30688f1c83c8b598261b9e65de899b90a14e4f04
Stefan Gordon <stefan@stefangordon.com>
2026-01-22 10:59:08 -0500

    Add helper functions and missing variables in mirage.h

--- components/mirage/mirage.h
+++ components/mirage/mirage.h
@@ -14,17 +14,19 @@ class MirageClimate : public climate_ir::ClimateIR {
             {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH},
             {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH}) {}
 
-  // --- THIS IS THE FIX FOR YOUR COMPILATION ERROR ---
-  // These allow the Python script to toggle modes on/off
+  // --- HELPER FUNCTIONS FOR PYTHON ---
   void set_supports_cool(bool supports_cool) { this->supports_cool_ = supports_cool; }
   void set_supports_heat(bool supports_heat) { this->supports_heat_ = supports_heat; }
   void set_supports_dry(bool supports_dry) { this->supports_dry_ = supports_dry; }
   void set_supports_fan_only(bool supports_fan_only) { this->supports_fan_only_ = supports_fan_only; }
-  // -------------------------------------------------
 
  protected:
   void transmit_state() override;
   bool on_receive(remote_base::RemoteReceiveData data) override;
+
+  // --- MISSING VARIABLES DEFINED HERE ---
+  uint32_t last_transmit_time_{0};
+  uint8_t swing_position{0};
 };
 
 }  // namespace mirage
