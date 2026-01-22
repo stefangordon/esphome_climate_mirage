e1a5610b6febfde93ba528cba94c9e89637e6064
Stefan Gordon <stefan@stefangordon.com>
2026-01-22 11:23:35 -0500

    Refactor climate component to use climate_ir schema
    Updated climate component to use climate_ir schema and removed transmitter_id and feature flags.

--- components/mirage/climate.py
+++ components/mirage/climate.py
@@ -1,45 +1,36 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate, climate_ir, remote_transmitter, remote_receiver, sensor
-from esphome.const import CONF_ID, CONF_SENSOR, CONF_NAME
+from esphome.components import climate_ir, remote_receiver, sensor
+from esphome.const import CONF_ID, CONF_SENSOR, CONF_RECEIVER_ID
 
+# Dependencies - climate_ir handles climate registration
 AUTO_LOAD = ["climate_ir"]
+CODEOWNERS = ["@stefangordon"]
 
 mirage_ns = cg.esphome_ns.namespace("mirage")
 MirageClimate = mirage_ns.class_("MirageClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate.climate_schema(MirageClimate).extend(
+# Use the standard climate_ir schema which already includes transmitter_id
+# and properly registers with Home Assistant
+CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(MirageClimate),
-        cv.Optional("transmitter_id"): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
-        cv.Optional("receiver_id"): cv.use_id(remote_receiver.RemoteReceiverComponent),
-        cv.Optional(CONF_SENSOR): cv.use_id(sensor.Sensor),
-        
-        # Feature flags
-        cv.Optional("supports_cool", default=True): cv.boolean,
-        cv.Optional("supports_heat", default=True): cv.boolean,
-        cv.Optional("supports_dry", default=True): cv.boolean,
-        cv.Optional("supports_fan_only", default=True): cv.boolean,
     }
 )
 
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    
+    # This is the key function - it:
+    # 1. Registers the component
+    # 2. Registers the climate entity (makes it show in HA/web UI)
+    # 3. Sets up the transmitter
+    # 4. Sets up supports_cool/heat from config
+    # 5. Sets up sensor if provided
     await climate_ir.register_climate_ir(var, config)
     
-    # --- FIX 1: CONNECT THE TRANSMITTER (The Smoking Gun) ---
-    if "transmitter_id" in config:
-        transmitter = await cg.get_variable(config["transmitter_id"])
-        cg.add(var.set_transmitter(transmitter))
-
-    # --- FIX 2: CONNECT THE SENSOR ---
-    if CONF_SENSOR in config:
-        sens = await cg.get_variable(config[CONF_SENSOR])
-        cg.add(var.set_sensor(sens))
-
-    # --- FIX 3: FORCE NAME & TRAITS ---
-    cg.add(var.set_name(config[CONF_NAME]))
-    cg.add(var.set_supports_cool(config["supports_cool"]))
-    cg.add(var.set_supports_heat(config["supports_heat"]))
-    cg.add(var.set_supports_dry(config["supports_dry"]))
-    cg.add(var.set_supports_fan_only(config["supports_fan_only"]))
+    # Set up the receiver if provided
+    if CONF_RECEIVER_ID in config:
+        receiver = await cg.get_variable(config[CONF_RECEIVER_ID])
+        cg.add(receiver.register_listener(var))
