68b994023512c3aa590f2ee01fd739e5b1893ec2
Stefan Gordon <stefan@stefangordon.com>
2026-01-22 10:58:10 -0500

    Refactor MirageClimate class for temperature and modes
    Updated temperature range and added support for additional modes.

--- components/mirage/mirage.h
+++ components/mirage/mirage.h
@@ -1,36 +1,30 @@
 #pragma once
 
 #include "esphome/components/climate_ir/climate_ir.h"
+#include "esphome/components/remote_base/mirage_protocol.h"
 
 namespace esphome {
 namespace mirage {
 
-/// Simple enum to represent models.
-
-// Temperature
-const float MIRAGE_TEMP_MAX = 32.0;
-const float MIRAGE_TEMP_MIN = 16.0;
-
 class MirageClimate : public climate_ir::ClimateIR {
  public:
-  uint8_t swing_position = 0;
   MirageClimate()
-      : climate_ir::ClimateIR(MIRAGE_TEMP_MIN, MIRAGE_TEMP_MAX, 1.0f, true, true,
-                              {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
-                               climate::CLIMATE_FAN_HIGH},
-                              {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL}) {}
+      : climate_ir::ClimateIR(
+            16.0, 30.0, 1.0, true, true,
+            {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH},
+            {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH}) {}
 
-  void setup() override {
-    climate_ir::ClimateIR::setup();
-  }
+  // --- THIS IS THE FIX FOR YOUR COMPILATION ERROR ---
+  // These allow the Python script to toggle modes on/off
+  void set_supports_cool(bool supports_cool) { this->supports_cool_ = supports_cool; }
+  void set_supports_heat(bool supports_heat) { this->supports_heat_ = supports_heat; }
+  void set_supports_dry(bool supports_dry) { this->supports_dry_ = supports_dry; }
+  void set_supports_fan_only(bool supports_fan_only) { this->supports_fan_only_ = supports_fan_only; }
+  // -------------------------------------------------
 
  protected:
-  /// Transmit via IR the state of this climate controller.
   void transmit_state() override;
-  /// Handle received IR Buffer
   bool on_receive(remote_base::RemoteReceiveData data) override;
-  /// Set the time of the last transmission.
-  int32_t last_transmit_time_{};
 };
 
 }  // namespace mirage
